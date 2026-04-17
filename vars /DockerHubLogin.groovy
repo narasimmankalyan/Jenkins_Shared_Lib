dcc11f07db00b01b407805a1362c9690c331d365
narasimmankalyan <158766757+narasimmankalyan@users.noreply.github.com>
2026-04-17 10:17:56 +0530

    Update DockerHubLogin.groovy

--- vars /DockerHubLogin.groovy
+++ vars /DockerHubLogin.groovy
@@ -1,1 +1,7 @@
-
+def call(String DockerHubUserName, String DockerHubPassword){
+  sh """
+  echo "-----------login_to_docker----------------"
+  echo docker login -u $(DockerHubUserName) --password $(DockerHubPassword)
+  echo "-------------login_successfully---------------"
+  """
+}
